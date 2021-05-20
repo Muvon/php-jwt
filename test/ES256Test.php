19048f7070bce26705c41a0824e0fe5cf5e5d74a
Don Hardman <don.hardman@muvon.io>
2021-05-20 14:37:48 +0700

    Add more data for test

--- test/ES256Test.php
+++ test/ES256Test.php
@@ -6,8 +6,8 @@ use PHPUnit\Framework\TestCase;
 class ES256Test extends TestCase {
   protected ES256 $jwt;
   protected function setUp(): void {
-    $public = '03fce314340b325d4949f4dd385ac81adcdc3f89a087b99abae96088606d201274';
-    $private = 'ba6bd7087653b7883a99cd3c8d385e30a188aa3204120e838072762f667e07de';
+    $public = '031587e192182b8a826d720bb81b944e2b979f7f27a149abd9b38bf00fb52cc218';
+    $private = 'c98a3420971233ee39bcca3d120f8ead3d90695dfedb26d8609bb7c0b451b14d';
     $this->jwt = ES256::create(
       $public,
       $private
@@ -15,8 +15,16 @@ class ES256Test extends TestCase {
   }
 
   public function testEncodeDecode() {
-    $data = ['hello', 'world'];
+    $data = array(
+      "data" => [
+          "name" => "ZiHang Gao",
+          "admin" => true
+      ],
+      "iss" => "http://example.org",
+      "sub" => "1234567890",
+    );
     $token = $this->jwt->encode($data);
+    var_dump($token);exit;
     $this->assertIsString($token);
     $decoded = $this->jwt->decode($token);
     $this->assertIsArray($decoded);
